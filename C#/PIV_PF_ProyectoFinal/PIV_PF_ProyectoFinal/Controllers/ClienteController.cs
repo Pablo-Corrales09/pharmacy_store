9b2315cf40d041dfac35b581736de1959bd668aa
Pablo-Corrales09 <devpablo.corrales@gmail.com>
2025-11-10 11:23:26 -0600

    Configuración de archivos MNV con C#

---
+++ C#/PIV_PF_ProyectoFinal/PIV_PF_ProyectoFinal/Controllers/ClienteController.cs
@@ -0,0 +1,80 @@
+﻿using PIV_PF_ProyectoFinal.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PIV_PF_ProyectoFinal.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: cliente
+        public ActionResult Index()
+        {
+            using (PIV_PF_ProyectoFinalEntities db = new PIV_PF_ProyectoFinalEntities())
+            {
+                var cliente = db.cliente.ToList();
+                return View(cliente);
+
+            }
+        }
+
+        public ActionResult ObtenerClientes()
+        {
+            using (PIV_PF_ProyectoFinalEntities db = new PIV_PF_ProyectoFinalEntities())
+            {
+                var usuarios = db.cliente.ToList();
+                return View("Index", usuarios);
+
+            }
+        }
+
+        [HttpPost]
+        public ActionResult CrearUsuario(cliente c)
+        {
+            try
+            {
+                using (PIV_PF_ProyectoFinalEntities db = new PIV_PF_ProyectoFinalEntities())
+                {
+                    db.cliente.Add(c);
+                    db.SaveChanges();
+                }
+                return RedirectToAction("Index");
+            }
+            catch { 
+                return View("Error");
+
+            }
+        }
+
+
+    }//Fin clienteController
+}//Fin namespace
+
+
+//public ActionResult CrearUsuario(cliente c)
+//{
+//    try
+//    {
+//        using (PIV_PF_ProyectoFinalEntities db = new PIV_PF_ProyectoFinalEntities())
+//        {
+//            db.cliente.Add(c);
+//            db.SaveChanges();
+//        }
+
+//        return Content("OK guardado!");
+//    }
+//    catch (DbEntityValidationException ex)
+//    {
+//        foreach (var eve in ex.EntityValidationErrors)
+//        {
+//            foreach (var ve in eve.ValidationErrors)
+//            {
+//                Response.Write("Propiedad: " + ve.PropertyName + " — Error: " + ve.ErrorMessage + "<br>");
+//            }
+//        }
+//        return Content("Error validación");
+//    }
+//}
---
+++ C#/PIV_PF_ProyectoFinal/PIV_PF_ProyectoFinal/Scripts/VistaClientes.js
@@ -0,0 +1,15 @@
+﻿document.addEventListener("DOMContentLoaded", function () {
+    const filas = document.querySelectorAll("table tbody tr");
+
+    filas.forEach(fila => {
+        fila.addEventListener("click", function () {
+
+            filas.forEach(f => f.classList.remove("selected"));
+            this.classList.add("selected");
+
+            let id = this.cells[0].innerText;
+            console.log("Cliente seleccionado:", id);
+        });
+    });
+});
+
